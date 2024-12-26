84f54d8d173b74f7bd611f9c9a2ced52b1ee488f
Yasserhatem <yasserhatem5555@gmail.com>
2024-12-26 19:29:03 +0200

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,43 +1,39 @@
-const request = require('request');
-const pick = require('lodash').pick;
+const axios = require('axios'); 
+const { pick } = require('lodash');
 const shouldCompress = require('./shouldCompress');
 const redirect = require('./redirect');
 const compress = require('./compress');
 const bypass = require('./bypass');
 const copyHeaders = require('./copyHeaders');
 
-function proxy(req, res) {
-  request.get(
-    req.params.url,
-    {
+async function proxy(req, res) {
+  try {
+    const response = await axios.get(req.params.url, {
       headers: {
         ...pick(req.headers, ['cookie', 'dnt', 'referer']),
         'user-agent': 'Bandwidth-Hero Compressor',
         'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
-        via: '1.1 bandwidth-hero'
+        via: '1.1 bandwidth-hero',
       },
       timeout: 10000,
+      responseType: 'arraybuffer',
       maxRedirects: 5,
-      encoding: null,
-      strictSSL: false,
-      gzip: true,
-      jar: true
-    },
-    (err, origin, buffer) => {
-      if (err || origin.statusCode >= 400) return redirect(req, res);
+    });
 
-      copyHeaders(origin, res);
-      res.setHeader('content-encoding', 'identity');
-      req.params.originType = origin.headers['content-type'] || '';
-      req.params.originSize = buffer.length;
+    copyHeaders(response, res);
+    res.setHeader('content-encoding', 'identity');
 
-      if (shouldCompress(req)) {
-        compress(req, res, buffer);
-      } else {
-        bypass(req, res, buffer);
-      }
+    req.params.originType = response.headers['content-type'] || '';
+    req.params.originSize = Buffer.byteLength(response.data);
+
+    if (shouldCompress(req)) {
+      compress(req, res, response.data);
+    } else {
+      bypass(req, res, response.data);
     }
-  );
+  } catch (err) {
+    redirect(req, res);
+  }
 }
 
 module.exports = proxy;
